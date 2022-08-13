92dda2b5bb70fee20bc64aa0a6b7510358d97a55
金雨 <10338105+AUH2O233@user.noreply.gitee.com>
2022-08-13 08:09:34 +0000

    update Back-end/models.py.
    Signed-off-by: 金雨 <10338105+AUH2O233@user.noreply.gitee.com>

--- Back-end/models.py
+++ Back-end/models.py
@@ -2,7 +2,6 @@ from exts import db
 from datetime import datetime
 
 
-
 class EmailCaptchaModel(db.Model):
     __tablename__ = "email_captcha"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -11,12 +10,11 @@ class EmailCaptchaModel(db.Model):
     create_time = db.Column(db.DateTime, default=datetime.now)
 
 
-#　登录
 class UserModel(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(100), nullable=False, unique=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(300), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
     # 记录创建账户的时间
     join_time = db.Column(db.DateTime, default=datetime.now)
