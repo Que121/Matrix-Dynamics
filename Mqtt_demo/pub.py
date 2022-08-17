10ea3f385ec5b025cb1e8eb53a4c805d80780544
Que121 <1095076407@qq.com>
2022-08-17 16:34:07 +0800

    小车回报报文解码完成

--- Mqtt_demo/pub.py
+++ Mqtt_demo/pub.py
@@ -13,10 +13,9 @@ client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 # 小车控制报文定义
 frame_header = "CTR"  # 头帧
-car_id_hundred = 0  # 小车id百位
 car_id_ten = 0  # 小车id十位
 car_id_bit = 1  # 小车id个位
-forward_speed = 5  # 前进速度 0-9
+forward_speed = 5  # 前进速度 0-9 1-5 0停车 1-5前进 6-9后退
 steering_speed = 4  # 转向速度 0直走 1-4（慢-快）左转 6-9右转（慢-快）
 power_on = 1  # 发1（并且无其它电机操作）解除电机锁 电机上电。
 water_pump = 0  # 1开水泵 0关水泵
@@ -38,10 +37,9 @@ def connect_mqtt():
 
 # 发布消息
 def publish(client):  
-    msg_count = 0
     while True:
-        time.sleep(1)
-        msg = f"{frame_header}{car_id_hundred}{car_id_ten}{car_id_bit}{forward_speed}{steering_speed}{power_on}{water_pump}{frame_end}"  # 待发布的消息
+        time.sleep(2)
+        msg = f"{frame_header}{car_id_ten}{car_id_bit}{forward_speed}{steering_speed}{power_on}{water_pump}{frame_end}"  # 待发布的消息
         result = client.publish(topic, msg)
         # result: [0, 1]
         status = result[0]
@@ -49,7 +47,7 @@ def publish(client):
             print(f"发给小车的控制报文为：{msg}")
         else:
             print(f"Failed to send message to topic {topic}")
-        msg_count += 1
+        
 
 
 def run():
--- Mqtt_demo/sub.py
+++ Mqtt_demo/sub.py
@@ -1,15 +1,24 @@
+from linecache import cache
+from nis import match
+from operator import index
 import random
+from socket import MsgFlag
+from unittest import case
 
 from paho.mqtt import client as mqtt_client
 
 # 设置 MQTT Broker 连接参数
 broker = '43.138.132.49'
 port = 1883
-topic = "car_control"
+topic = "car_state"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
+msg = "default"
+car_state = "ID:01_GPS:E11919.42N2605.30GTime:00:00:00_OFF_OFF_Wt:000_D:0.00_P:0.0Y:0.0B:000_T:31.7_H:49.0_Ready"
 
 # MQTT连接函数
+
+
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
@@ -22,13 +31,89 @@ def connect_mqtt() -> mqtt_client:
     client.connect(broker, port)
     return client
 
+
 # 订阅消息
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        
+        global car_state
+        car_state = msg.payload.decode()  # 接收
+        print(f"{msg.payload.decode()}")
+        decode()
+        print("\n")
     client.subscribe(topic)
     client.on_message = on_message
+
+
+# 小车回报报文解码定义
+car_gps_longitude = 0
+car_gps_latitude = 0
+car_motor = 0
+car_water = 0
+car_distance = 0
+car_pitch = 0
+car_battery = 0
+car_temperature = 0
+car_humidity = 0
+car_end = 0  # 帧尾
+
+
+# 解码
+def decode():
+    global car_header, car_gps_longitude, car_gps_latitude, \
+        car_gps_GTime, car_motor, car_water, car_distance,  \
+        car_pitch, car_yaw, car_battery, car_temperature,  \
+        car_humidity, car_end
+# =======================================================================================
+    for index_header in range(len(car_state[:(len(car_state)-5)])):
+        if ((f"{car_state[index_header]}{car_state[index_header+1]}") == "ID"):
+            for index_end in range(len(car_state[index_header:(len(car_state)-5)])):
+                if ((f"{car_state[index_end]}{car_state[index_end+1]}") == "GP"):
+                    car_header = car_state[index_header+3:index_end-1]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "E"):
+            for index_end in range(len(car_state[index_header:(len(car_state)-5)])):
+                if ((f"{car_state[index_end]}") == "N"):
+                    car_gps_longitude = car_state[index_header:index_end]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "N"):
+            for index_end in range(len(car_state[index_header:(len(car_state)-5)])):
+                if ((f"{car_state[index_end]}{car_state[index_end+1]}") == "GT"):
+                    car_gps_latitude = car_state[index_header:index_end]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}{car_state[index_header+1]}") == "GT"):
+            car_gps_GTime = car_state[index_header+6:index_header+14]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}{car_state[index_header+1]}") == "me"):
+            for index_end in range(len(car_state[index_header:(len(car_state)-5)])):
+                if ((f"{car_state[index_end]}{car_state[index_end+1]}") == "Wt"):
+                    car_motor = car_state[index_header+12:index_end-1]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}{car_state[index_header+1]}") == "Wt"):
+            car_water = car_state[index_header+3:index_header+6]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "D"):
+            car_distance = car_state[index_header+2:index_header+6]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "P"):
+            car_pitch = car_state[index_header+2:index_header+5]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "Y"):
+            car_yaw = car_state[index_header+2:index_header+5]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "B"):
+            car_battery = car_state[index_header+2:index_header+5]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "T"):
+            car_temperature = car_state[index_header+2:index_header+6]
+# =======================================================================================
+        elif ((f"{car_state[index_header]}") == "H"):
+            car_humidity = car_state[index_header+2:index_header+6]
+    car_end = car_state[(len(car_state)-5):]
+# =======================================================================================
+    print(f"{car_header}\n{car_gps_longitude}\n{car_gps_latitude} \
+      \n{car_gps_GTime}\n{car_motor}\n{car_water} \
+      \n{car_distance}\n{car_pitch}\n{car_yaw} \
+      \n{car_battery}\n{car_temperature}\n{car_humidity}\n{car_end}")
 
 
 def run():
